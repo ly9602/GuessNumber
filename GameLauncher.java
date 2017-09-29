46190b2f9ddc1289ca4295d30ed2b01244b81ce3
墨基 <liya9602@qq.com>
2017-09-29 23:41:21 +0800

    Guessnumber
    1

--- GameLauncher.java
+++
@@ -1,8 +0,0 @@
-
-public class GameLauncher {
-	public static void main (String[] args) {
-		guessgame game=new guessgame();
-		game.startGame();
-	}
-
-}
--- Player.java
+++ Player.java
@@ -1,8 +1,7 @@
-
-public class Player {
-	int number=0;
-	public void guess() {
-		number=(int)(Math.random()*10);
-		System.out.println("I'm guessing "+number);
-	}
+public class player {
+    int number=0;
+    public void guess() {
+        number=(int)(Math.random()*10);
+        System.out.println("I'm guessing "+number);
+    }
 }
